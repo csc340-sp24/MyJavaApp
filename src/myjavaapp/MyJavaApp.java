ed2f242814ba6aa6443766e78ec26a9e574cca03
csc340sp24 <csc340sp24@gmail.com>
2024-01-11 16:29:38 -0500

    I made some changes

--- src/myjavaapp/MyJavaApp.java
+++ src/myjavaapp/MyJavaApp.java
@@ -11,6 +11,7 @@ public class MyJavaApp {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Added A NEW LINE!!!!");
     }
     
 }
