673da3fec22118c1302222519a3ff29273c7a50d
csc340sp24 <csc340sp24@gmail.com>
2024-01-11 16:18:55 -0500

    initial commit

---
+++ src/myjavaapp/MyJavaApp.java
@@ -0,0 +1,16 @@
+package myjavaapp;
+
+/**
+ *
+ * @author sunny
+ */
+public class MyJavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
